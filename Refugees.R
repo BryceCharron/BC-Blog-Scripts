314a4c2d42119f68a705eb124a9c1385ce486338
BryceCharron <bcharron@middlebury.edu>
2025-07-14 13:25:33 -0400

    Add files via upload

--- Refugees.R
+++ Refugees.R
@@ -39,10 +39,12 @@ ref_total <- read_csv("C:/Users/wb647336/OneDrive - WBG/Documents/Refugee Blog P
   filter(Year == "2023") %>% 
   select(Year, `Country of Asylum`, `Country of Origin`, `Country of Asylum ISO`, `Country of Origin ISO`,
          Refugees)
+
+# read in UNHCR income classification df
 ref_class <- read_excel("C:/Users/wb647336/OneDrive - WBG/Documents/Refugee Blog Post/CLASS.xlsx")
 
-# clean UNHCR df, calculate percentages for total refugee flows
-ref_total_clean <- ref_total %>%
+# create UNHCR df - count of refugees moving b/t income groups
+ref_unhcr <- ref_total %>%
   left_join(ref_class, by = c("Country of Asylum ISO" = "Code")) %>%
   left_join(ref_class, by = c("Country of Origin ISO" = "Code")) %>%
   select(Year, 
@@ -60,9 +62,9 @@ ref_total_clean <- ref_total %>%
   summarise(total_refugees = sum(Refugees), .groups = "drop") 
 
 # write .csv
-write.csv(ref_total_clean, "ref_total_clean.csv", row.names = F)
+write.csv(ref_unhcr, "ref_unhcr.csv", row.names = F)
 
-# create UNRWA flow df
+# create UNRWA df - count of refugees moving from West Bank & Gaza to income classifications
 ref_unrwa <- ref_asylum %>% 
   left_join(ref_origin, by = "Country Name") %>% 
   rename(total_refugees = `2023.x`,
@@ -76,16 +78,17 @@ ref_unrwa <- ref_asylum %>%
 # write .csv
 write.csv(ref_unrwa, "ref_unrwa.csv", row.names = F)
 
-# combine dfs
-ref_all <- bind_rows(ref_total_clean, ref_unrwa)
+# combine UNHCR and UNRWA
+ref_combined <- bind_rows(ref_unhcr, ref_unrwa) 
 
-dest_totals <- ref_all %>%
+ref_dest <- ref_combined %>%
   group_by(asylum_income_group) %>%
   summarise(dest_total = sum(total_refugees), .groups = "drop") %>%
   mutate(dest_total_pct = 100 * dest_total / sum(dest_total))
 
-ref_final <- ref_all %>%
-  left_join(dest_totals, by = "asylum_income_group") %>%
+# form final df with UNHCR and UNRWA data with flows and percentages (for sankey)
+ref_final <- ref_combined %>%
+  left_join(ref_dest, by = "asylum_income_group") %>%
   group_by(origin_income_group) %>%
   mutate(origin_total = sum(total_refugees)) %>%
   ungroup() %>%
@@ -95,48 +98,7 @@ ref_final <- ref_all %>%
 # write .csv
 write.csv(ref_final, "ref_final.csv", row.names = F)
 
-
-# sankey plot per code from Sinae
-stratum_fill <- c(rev(c("HIC","UMC","LMC","LIC")),
-  rev(c("HIC","UMC","LMC","LIC")))
-
-# Set directories
-dir <- getwd()
-sdg.dir <- paste0(dir,"/SDG_Atlas/addChart2022/")
-input.dir <- paste0(sdg.dir, "inputs/" )
-charts.dir <- paste0(sdg.dir, "charts/" )
-
-source("C:/Users/wb647336/OneDrive - WBG/Documents/Refugee Blog Post/styles.R")
-style <- style_atlas()
-
-ref_final %>% 
-  ggplot(aes(axis1 = origin_income_group, axis2 = asylum_income_group, y = total_refugees)) +
-  scale_x_discrete(limits = c("origin_income_group", "asylum_income_group"), labels=c("Origin", "Destination"),
-                   expand = c(0.17, 0.17), position = "top") +
-  geom_alluvium(aes(fill = origin_income_group), width=1/10) +
-  scale_y_continuous(expand=c(0,1))+
-  scale_fill_manual(values=style$colors$incomes.alt) +
-  geom_stratum(width=1/10, fill = style$colors$incomes.alt[stratum_fill], color=NA) +
-  geom_text(aes(label=str_wrap_lines(style$labels$income.lb[origin_income_group], 2)),
-    stat="stratum",
-    hjust = 1,
-    vjust=0.8,
-    nudge_x = -0.07,
-    family = style$theme()$text$family,
-    size = style$gg_text_size*0.8,
-    lineheight = 0.75) +
-  geom_text(aes(label=str_wrap_lines(style$labels$income.lb[asylum_income_group], 2)),
-    stat="stratum",
-    hjust = 0,
-    nudge_x = 0.07,
-    family = style$theme()$text$family,
-    size = style$gg_text_size*0.8,
-    lineheight = 0.75) +
-  style$theme() +
-  theme(axis.text.y=element_blank(),
-        panel.grid = element_blank())
-
-# stacked area chart
+# let's form a df for a stacked line area chart in flourish
 years <- as.character(1970:2023)
 incomeGroups <- c("Low income", "Lower middle income",
                   "Upper middle income",
@@ -152,5 +114,5 @@ ref_area_data <- read_csv("C:/Users/wb647336/OneDrive - WBG/Documents/Refugee Bl
   pivot_wider(names_from = `Country Name`,
               values_from = Value)
 
-
+# write.csv
 write.csv(ref_area_data, "ref_area_df.csv", row.names = F)
