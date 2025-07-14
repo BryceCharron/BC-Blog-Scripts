514f1751ae9c6c95705c78c5c0267c6efc127b27
BryceCharron <bcharron@middlebury.edu>
2025-07-14 13:38:19 -0400

    Add files via upload

--- Remittances.R
+++ Remittances.R
@@ -48,7 +48,7 @@ remit_gdp_clean_all <- remit_gdp %>%
 # write.csv
 write.csv(remit_gdp_clean_all, "remit_gdp_clean_all.csv", row.names = F)
 
-# merge remittance points layer df from flourish
+# form remittance points layer df for flourish
 remit_points <- read_csv("C:/Users/wb647336/OneDrive - WBG/Documents/Remittances Blog Post/remit_points.csv") %>% 
   left_join(remit_gdp_clean_all, by = c("Name" = "Country Name")) %>%
   select(Region.x, Name, Longitude, Latitude, `Population (2019)`, IncomeGroup.x, Color.x, `2023.x`) %>% 
@@ -57,12 +57,10 @@ remit_points <- read_csv("C:/Users/wb647336/OneDrive - WBG/Documents/Remittances
 # which are top 5 countries w/highest gdp contribution by remittance flows?
 remit_points %>% 
   filter(`2023.x` > 25) %>% 
-  arrange(desc(`2023.x`)) %>% 
-  head(10)
+  arrange(desc(`2023.x`)) 
 
 # write .csv
 write.csv(remit_points, "remit_points.csv", row.names = F)
-
 
 ### remittance cost slope chart (as a %)
 years <- paste0("X", 2016:2023)
@@ -82,8 +80,8 @@ remit_cost_clean <- remit_cost %>%
 # how many countries still have costs greater than 7%?
 remit_cost_clean %>% 
   filter(X2023 >= 7) %>% 
-  count(Country.Name) %>%
-  arrange(desc(n)) 
+  count(Country.Name) %>% 
+  arrange(desc(Country.Name)) 
 
 # write .csv
 write.csv(remit_cost_clean, "remit_cost_clean.csv", row.names = F)
